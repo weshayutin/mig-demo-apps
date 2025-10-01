782caa5a18d37b90610f09d56a5555e912a41029
Wesley Hayutin <weshayutin@gmail.com>
2025-10-01 09:16:39 -0600

    modernize server.js

--- apps/parks-app/src/server.js
+++ apps/parks-app/src/server.js
@@ -1,8 +1,8 @@
-var restify = require('restify'),
-    fs      = require('fs'),
-    config  = require('./bin/config.js'),
-    db      = require('./bin/db.js');
-var app     = restify.createServer();
+const restify = require('restify'),
+      fs      = require('fs'),
+      config  = require('./bin/config.js'),
+      db      = require('./bin/db.js');
+const app     = restify.createServer();
 
 // Initialize database asynchronously (will retry when MongoDB is available)
 db.initDB('keepAlive').catch(err => {
@@ -25,24 +25,60 @@ app.use(function(req, res, next) {
   }
 })
 
-// Routes
-app.get('/parks/within', db.selectBox);
-app.get('/parks', db.selectAll);
-app.get('/status', function (req, res, next)
-{
-  res.send("{status: 'ok'}");
+// Routes - wrap async functions for Restify v11
+app.get('/parks/within', async (req, res) => {
+  try {
+    await db.selectBox(req, res);
+  } catch (err) {
+    res.send(500, { error: err.message });
+  }
 });
 
-app.get('/', function (req, res, next)
-{
-  var data = fs.readFileSync(__dirname + '/index.html');
-  res.status(200);
-  res.header('Content-Type', 'text/html');
-  res.end(data.toString().replace(/host:port/g, req.header('Host')));
+app.get('/parks', async (req, res) => {
+  try {
+    await db.selectAll(req, res);
+  } catch (err) {
+    res.send(500, { error: err.message });
+  }
+});
+app.get('/status', (req, res) => {
+  res.send({ status: 'ok' });
 });
 
-app.get(/\/(css|js|img)\/?.*/, restify.plugins.serveStatic({directory: __dirname+'/static/'}));
+app.get('/', (req, res) => {
+  try {
+    const data = fs.readFileSync(__dirname + '/index.html');
+    res.status(200);
+    res.header('Content-Type', 'text/html');
+    res.end(data.toString().replace(/host:port/g, req.header('Host')));
+  } catch (err) {
+    res.send(500, { error: 'Failed to load index.html' });
+  }
+});
 
-app.listen(config.get('PORT'), config.get('IP'), function () {
-  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT'))
+// Static file serving for Restify v11
+app.get(/\/(css|js|img)\/?.*/, (req, res, next) => {
+  const path = require('path');
+  const filePath = path.join(__dirname, 'static', req.url);
+  
+  try {
+    const data = fs.readFileSync(filePath);
+    const ext = path.extname(filePath);
+    const contentType = {
+      '.css': 'text/css',
+      '.js': 'application/javascript',
+      '.jpg': 'image/jpeg',
+      '.png': 'image/png',
+      '.gif': 'image/gif'
+    }[ext] || 'application/octet-stream';
+    
+    res.header('Content-Type', contentType);
+    res.send(200, data);
+  } catch (err) {
+    res.send(404, { error: 'File not found' });
+  }
+});
+
+app.listen(config.get('PORT'), config.get('IP'), () => {
+  console.log(`Listening on ${config.get('IP')}, port ${config.get('PORT')}`);
 });
